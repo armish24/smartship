52dd03c7dc87d0b9f0bcf1b1e2841d2cc6f15a62
armish24 <aarpit24@gmail.com>
2019-09-28 19:59:12 +0530

    secondly changes partially complete

--- www/js/index.js
+++ www/js/index.js
@@ -6,12 +6,33 @@ document.getElementById("cameraTakePicture").addEventListener
    function cameraTakePicture() { 
    navigator.camera.getPicture(onSuccess, onFail, {  
       quality: 50, 
+      saveToPhotoAlbum: 1,
       destinationType: Camera.DestinationType.DATA_URL 
    });  
    
    function onSuccess(imageData) { 
       var image = document.getElementById('myImage'); 
       image.src = "data:image/jpeg;base64," + imageData; 
+      console.log(imageData);
+      console.log("\n");
+      console.log("data:image/jpeg;base64," + imageData);
+
+      $.post("https://shellkore.serveo.net/", { data : imageData },function(output, status){
+      
+   
+    console.log(output);
+    console.log(typeof(output));
+
+      document.getElementById("id-d").innerHTML = output;
+
+
+     
+   
+
+    });
+
+
+    
    }  
    
    function onFail(message) { 
